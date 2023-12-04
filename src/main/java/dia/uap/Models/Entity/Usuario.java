9ac54d87f6d702233d0a6f46b7b83ed71a0e6d21
abrahan24 <asaraviacoca@gmail.com>
2023-12-04 16:20:18 -0400

    update

--- src/main/java/dia/uap/Models/Entity/Usuario.java
+++ src/main/java/dia/uap/Models/Entity/Usuario.java
@@ -1,6 +1,11 @@
 package dia.uap.Models.Entity;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +14,11 @@ import lombok.Setter;
 @Table(name = "usuario")
 @Setter
 @Getter
-public class Usuario {
+public class Usuario implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_usuario;
+    private String nom_usuario;
+    private String contrasena;
 }
