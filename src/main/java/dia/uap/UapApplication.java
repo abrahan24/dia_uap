ed3ce8bf5e00fcf29621f282f27cb0e767c76fda
abrahan24 <asaraviacoca@gmail.com>
2023-12-04 16:17:13 -0400

    update

---
+++ src/main/java/dia/uap/UapApplication.java
@@ -0,0 +1,13 @@
+package dia.uap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dia/uap/UapApplicationTests.java
@@ -0,0 +1,13 @@
+package dia.uap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
