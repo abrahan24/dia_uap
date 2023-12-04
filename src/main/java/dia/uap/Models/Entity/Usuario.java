18ac4d630c0b8aa472c210b07561c412f4674bc3
abrahan24 <asaraviacoca@gmail.com>
2023-12-04 16:18:42 -0400

    Update

---
+++ src/main/java/dia/uap/Models/Entity/Usuario.java
@@ -0,0 +1,14 @@
+package dia.uap.Models.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Setter
+@Getter
+public class Usuario {
+    
+}
